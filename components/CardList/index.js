0cbc2c1b9890d42dc70b3dd7f3d27e3c155a3a7a
Carlos Flores Nava <carlos.flores.nava@hotmail.com>
2022-08-12 21:46:11 -0500

    feat(youtube-api): fetch videos from Youtube Search API on the server side (SSR)

--- components/CardList/index.js
+++ components/CardList/index.js
@@ -1,13 +1,18 @@
 import Card from '../Card';
 import styles from './card-list.module.css';
 
-export default ({ title, videos, size }) => {
+export default ({ title, videos = [], size }) => {
   return (
     <section className={styles.container}>
       <h2 className={styles.title}>{title}</h2>
       <div className={styles.cardWrapper}>
         {videos.map((video, index) => (
-          <Card index={index} imgUrl={video.imgUrl} size={size} />
+          <Card
+            key={video.id}
+            index={index}
+            imgUrl={video.imgUrl}
+            size={size}
+          />
         ))}
       </div>
     </section>
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,17 @@
 import Head from 'next/head';
 import Banner from '../components/Banner';
-import Card from '../components/Card';
 import CardList from '../components/CardList';
 import Navbar from '../components/Navbar';
+import { getVideos } from '../lib/videos';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
-  const disneyVideos = [
-    {
-      imgUrl: "/static/clifford.webp",
-    },
-    {
-      imgUrl: "/static/clifford.webp",
-    },
-    {
-      imgUrl: "/static/clifford.webp",
-    },
-  ];
+export async function getServerSideProps() {
+  const disneyVideos = await getVideos();
 
+  return { props: { disneyVideos } };
+}
+
+export default function Home({ disneyVideos }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -31,11 +25,11 @@ export default function Home() {
         subTitle='a very cute dog'
         imgUrl='/static/clifford.webp'
       />
-      <Navbar username="ankita@ank.com" />
+      <Navbar username='ankita@ank.com' />
       <div className={styles.cardListWrapper}>
-        <CardList title="Disney" videos={disneyVideos} size='large' />
-        <CardList title="My List" videos={disneyVideos} size='medium' />
-        <CardList title="My List" videos={disneyVideos} size='small' />
+        <CardList title='Disney' videos={disneyVideos} size='large' />
+        <CardList title='My List' videos={disneyVideos} size='medium' />
+        <CardList title='My List' videos={disneyVideos} size='small' />
       </div>
     </div>
   );
