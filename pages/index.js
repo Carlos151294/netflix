a9979c9bac2eaf8f0dec9259750ca1006820b33e
Carlos Flores Nava <carlos.flores.nava@hotmail.com>
2022-09-08 09:13:06 -0500

    feat(sections): add productivity, travel and popular videos sections

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,26 @@ import Head from 'next/head';
 import Banner from '../components/Banner';
 import CardList from '../components/CardList';
 import Navbar from '../components/Navbar';
-import { getVideos } from '../lib/videos';
+import { getPopularVideos, getVideos } from '../lib/videos';
 import styles from '../styles/Home.module.css';
 
 export async function getServerSideProps() {
-  const disneyVideos = await getVideos();
+  const disneyVideos = await getVideos('disney trailer');
+  const productivityVideos = await getVideos('productivity');
+  const travelVideos = await getVideos('travel');
+  const popularVideos = await getPopularVideos('travel');
 
-  return { props: { disneyVideos } };
+  return {
+    props: { disneyVideos, productivityVideos, travelVideos, popularVideos },
+  };
 }
 
-export default function Home({ disneyVideos }) {
+export default function Home({
+  disneyVideos,
+  travelVideos,
+  productivityVideos,
+  popularVideos,
+}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -20,16 +30,23 @@ export default function Home({ disneyVideos }) {
         <link rel='icon' href='/nficon2016.ico' />
       </Head>
 
-      <Banner
-        title='Clifford the red dog'
-        subTitle='a very cute dog'
-        imgUrl='/static/clifford.webp'
-      />
-      <Navbar username='ankita@ank.com' />
-      <div className={styles.cardListWrapper}>
-        <CardList title='Disney' videos={disneyVideos} size='large' />
-        <CardList title='My List' videos={disneyVideos} size='medium' />
-        <CardList title='My List' videos={disneyVideos} size='small' />
+      <div className={styles.main}>
+        <Banner
+          title='Clifford the red dog'
+          subTitle='a very cute dog'
+          imgUrl='/static/clifford.webp'
+        />
+        <Navbar username='ankita@ank.com' />
+        <div className={styles.cardListWrapper}>
+          <CardList title='Disney' videos={disneyVideos} size='large' />
+          <CardList title='Travel' videos={travelVideos} size='small' />
+          <CardList
+            title='Productivity'
+            videos={productivityVideos}
+            size='medium'
+          />
+          <CardList title='Popular' videos={popularVideos} size='small' />
+        </div>
       </div>
     </div>
   );
